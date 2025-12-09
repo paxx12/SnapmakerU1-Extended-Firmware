eacdd7fd4e697abf0e566a0cd68e9bc8c008da9d
paxx12 <245230251+paxx12@users.noreply.github.com>
2025-12-09 23:15:51 +0100

    Store version in a firmware

--- overlays/store-version/scripts/01_store_version.sh
+++ overlays/store-version/scripts/01_store_version.sh
@@ -11,5 +11,6 @@ if [[ -n "$GIT_VERSION" ]]; then
   ABBRV=$(git describe --abbrev --always)
 
   # 0.9.0-paxx12-1-gabcdef0
-  echo "${GIT_VERSION#v}-${ABBRV}" > "$ROOTFS_DIR/etc/CUSTOM_VERSION"
+  echo "${GIT_VERSION#v}" > "$ROOTFS_DIR/etc/VERSION"
+  echo "${GIT_VERSION#v}_$(date +%Y%m%d%H%M%S)_${ABBRV}" > "$ROOTFS_DIR/etc/CUSTOMVERSION"
 fi
