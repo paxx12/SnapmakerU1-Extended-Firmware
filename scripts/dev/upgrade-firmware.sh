2cfbff7be15b957753b09c822cc5b18fb95c855d
paxx12 <245230251+paxx12@users.noreply.github.com>
2025-12-28 22:42:56 +0100

    Remove DEVEL parameter from upgrade-firmware script
    Simplify the script by removing the unused DEVEL build option.

--- scripts/dev/upgrade-firmware.sh
+++ scripts/dev/upgrade-firmware.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-if [[ $# -lt 2 ]]; then
-  echo "usage: $0 <user@ip> <profile> [DEVEL=0|1]"
+if [[ $# -ne 2 ]]; then
+  echo "usage: $0 <user@ip> <profile>"
   exit 1
 fi
 
 SSH_HOST="$1"
 PROFILE="$2"
-case "${3,,}" in
-  devel|devel=1|devel=true) DEVEL=1 ;;
-  *) DEVEL=0 ;;
-esac
 shift 2
 
 set -xe
 
 rm -rf "firmware/firmware_$PROFILE.bin" "tmp/firmware"
-make build OUTPUT_FILE=firmware/firmware_$PROFILE.bin PROFILE="$PROFILE" DEVEL="$DEVEL"
+make build OUTPUT_FILE=firmware/firmware_$PROFILE.bin PROFILE="$PROFILE"
 scp "tmp/firmware/update.img" "$SSH_HOST:/tmp/"
 ssh "$SSH_HOST" /home/lava/bin/systemUpgrade.sh upgrade soc /tmp/update.img
