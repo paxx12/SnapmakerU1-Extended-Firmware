2ac4cc2ff69f894dc78cb74f05b63842e810c3a9
Justin Hayes <52832301+justinh-rahb@users.noreply.github.com>
2026-01-08 15:35:26 -0500

    Update v4l2-ctrls startup and pin upstream

--- overlays/firmware-extended/12-camera-v4l2-mpp/scripts/01-v4l2-mpp.sh
+++ overlays/firmware-extended/12-camera-v4l2-mpp/scripts/01-v4l2-mpp.sh
@@ -2,8 +2,8 @@
 
 ROOT_DIR="$(realpath "$(dirname "$0")/../../../..")"
 
-GIT_URL=https://github.com/paxx12/v4l2-mpp.git
-GIT_SHA=f2f4e34788e4d88377d0f6e17838b465524f2aa7
+GIT_URL=https://github.com/justinh-rahb/v4l2-mpp.git
+GIT_SHA=2c48e79dd431ceea3df2c77aeb906c4a807e9868
 
 if [[ $# -ne 1 ]]; then
   echo "Usage: $0 <rootfs-dir>"
@@ -44,5 +44,5 @@ file "$1/usr/local/bin/stream-rtsp"
 file "$1/usr/local/bin/stream-webrtc"
 file "$1/usr/local/bin/stream-snap-mqtt.py"
 file "$1/usr/local/bin/stream-http.py"
-
+file "$1/usr/local/bin/v4l2-ctrls.py"
 echo ">> v4l2-mpp installation completed successfully."
