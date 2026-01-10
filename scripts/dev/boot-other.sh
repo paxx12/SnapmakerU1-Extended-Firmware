2f58eac6827bf9010175f213d49b8bf04da02cd0
paxx12 <245230251+paxx12@users.noreply.github.com>
2026-01-10 11:43:01 +0100

    Another iteration

--- scripts/dev/boot-other.sh
+++ scripts/dev/boot-other.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]]; then
-  echo "usage: $0 <user@ip> <profile>"
+if [[ $# -ne 1 ]]; then
+  echo "usage: $0 <user@ip>"
   exit 1
 fi
 
 SSH_HOST="$1"
-PROFILE="$2"
-shift 2
 
 set -xe
 
-sshpass -p snapmaker ssh -t "$SSH_HOST" sudo /opt/lava/updateEngine --misc=other --reboot
+sshpass -p snapmaker ssh -t "$SSH_HOST" /opt/lava/bin/updateEngine --misc=other --reboot
