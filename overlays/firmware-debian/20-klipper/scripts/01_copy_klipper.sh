7617c7df144f1d866ef22a7091398a591dcebeb7
paxx12 <245230251+paxx12@users.noreply.github.com>
2026-01-10 15:06:10 +0100

    Copy klipper

---
+++ overlays/firmware-debian/20-klipper/scripts/01_copy_klipper.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $0 <rootfs-dir>"
+  exit 1
+fi
+
+ROOT_DIR="$(realpath "$(dirname "$0")/../../../..")"
+ROOTFS_DIR="$(realpath "$1")"
+ORG_ROOTFS_DIR="${ROOTFS_DIR}.org"
+
+set -e
+
+mkdir_chroot() {
+  for dir; do
+    echo ">> Creating directory $dir in chroot..."
+    mkdir -p "$ROOTFS_DIR/$dir"
+  done
+}
+
+copy_chroot() {
+  local target="$1"
+  shift
+
+  echo ">> Copying to $target the $@..."
+  for src; do
+    cp -rv -L --remove-destination "$ORG_ROOTFS_DIR/$src" "$ROOTFS_DIR/$target"
+  done
+}
+
+in_chroot() {
+  "$ROOT_DIR/scripts/helpers/chroot_firmware.sh" "$ROOTFS_DIR" bash -c "$@"
+}
+
+echo ">> Copying Klipper from /home/lava/klipper to /opt/klipper..."
+mkdir_chroot /opt/klipper
+copy_chroot /opt/klipper/ /home/lava/klipper/.
+
+echo ">> Creating virtual environment in /opt/klipper..."
+in_chroot 'python3 -m venv /opt/klipper/venv'
+
+echo ">> Installing klipper requirements..."
+in_chroot '/opt/klipper/venv/bin/pip3 install --upgrade pip'
+in_chroot '/opt/klipper/venv/bin/pip3 install -r /opt/klipper/scripts/klippy-requirements.txt'
+
+echo ">> Setting ownership to lava:lava..."
+in_chroot 'chown -R lava:lava /opt/klipper'
+
+echo ">> Klipper installation complete"
