2eca31d0b878088f845dcff6f2ce65a53ff9f987
paxx12 <245230251+paxx12@users.noreply.github.com>
2026-01-11 12:54:04 +0100

    AI detection 2

--- overlays/firmware-debian/44-fake-camera-agent/scripts/01_install_rknn.chroot.sh
+++ overlays/firmware-debian/44-fake-camera-agent/scripts/01_install_rknn.chroot.sh
@@ -4,4 +4,4 @@ set -e
 
 echo ">> Installing RKNN Toolkit Lite2 in venv..."
 /opt/venv/bin/pip3 install https://github.com/airockchip/rknn-toolkit2/raw/refs/heads/master/rknn-toolkit-lite2/packages/rknn_toolkit_lite2-2.3.2-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl
-/opt/venv/bin/pip3 install opencv-python-headless numpy
+/opt/venv/bin/pip3 install opencv-python-headless 'numpy<2.0.0'
