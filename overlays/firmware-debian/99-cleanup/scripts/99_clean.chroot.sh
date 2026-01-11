b3ec76afe18b77aced315a3fe3c5506b2c3dc6af
paxx12 <245230251+paxx12@users.noreply.github.com>
2026-01-11 12:08:54 +0100

    Perform risky cleanup

--- overlays/firmware-debian/99-cleanup/scripts/99_clean.chroot.sh
+++ overlays/firmware-debian/99-cleanup/scripts/99_clean.chroot.sh
@@ -8,17 +8,31 @@ apt-get autopurge -y \
   cpp-14-aarch64-linux-gnu \
   gcc-14-aarch64-linux-gnu \
   g++-14-aarch64-linux-gnu \
+  binutils-aarch64-linux-gnu \
   libgcc-14-dev \
   vim-runtime \
   git
 
+apt-get autopurge -y perl libperl*
+apt-get autopurge -y python3-setuptools
+apt-get autopurge -y packagekit libgstreamer*
+
 # bookworm cleanup
 apt-get autoremove -y libpython3.11-dev
 
+# remove python bytecode files
+find / -name '*.pyc' -delete
+
+# risky cleanup (breaks the system dependencies)
+rm -rf /usr/include/*
+rm -rf /usr/share/doc
+rm -rf /usr/share/doc-base
+rm -rf /usr/share/man
+
 # general cleanup
-rm -rf "/var/cache/apt/"
-rm -rf "/var/lib/apt/lists/"
+rm -rf /var/cache/apt
+rm -rf /var/lib/apt/lists
 find "/var/log" -type f -delete
 rm -rf "/tmp/"*
-rm -rf "/usr/share/locale/"
-rm -rf "/etc/machine-id"
+rm -rf /usr/share/locale
+rm -rf /etc/machine-id
