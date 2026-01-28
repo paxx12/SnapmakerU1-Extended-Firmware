edbee5b64aeb949844e84e55a5844b624efbcf66
paxx12 <245230251+paxx12@users.noreply.github.com>
2026-01-28 12:22:43 +0100

    Add `.chroot.sh` scripts

--- scripts/helpers/chroot_firmware.sh
+++ scripts/helpers/chroot_firmware.sh
@@ -1,22 +1,44 @@
 #!/usr/bin/env bash
 
+if [[ -z "$CREATE_FIRMWARE" ]]; then
+  echo "Error: This script should be run within the create_firmware.sh environment."
+  exit 1
+fi
+
 if [[ $# -lt 2 ]]; then
   echo "Usage: $0 <rootfs> <cmd> [args...]"
   exit 1
 fi
 
 ROOTFS="$(realpath "$1")"
+ROOT_CACHE="$CACHE_DIR/root-cache"
 shift
 
 cd "$ROOTFS"
 
-if [[ -e ./etc/resolv.conf ]]; then
-  echo "[!] The ./etc/resolv.conf file already exists. Failing."
-  exit 1
-fi
-
+[[ -e ./etc/resolv.conf ]] && mv ./etc/resolv.conf{,.bak}
 echo "nameserver 1.1.1.1" > ./etc/resolv.conf
 
-chroot "$ROOTFS" "$@"
+cleanup() {
+  rm -f ./etc/resolv.conf
+  [[ -e ./etc/resolv.conf.bak ]] && mv ./etc/resolv.conf{.bak,}
+  umount -l ./proc
+  umount -l ./sys
+  umount -l ./dev
+  umount -l ./root/.cache
+  umount -l ./firmware-root
+  rmdir ./firmware-root
+}
 
-rm -f ./etc/resolv.conf
+trap cleanup EXIT
+
+mkdir -p "$ROOT_CACHE" ./root/.cache
+mkdir -p ./firmware-root
+
+mount --bind "$ROOT_CACHE" ./root/.cache
+mount -t proc /proc ./proc
+mount --bind /sys ./sys
+mount --bind /dev ./dev
+mount --bind "$ROOT_DIR" ./firmware-root
+
+chroot "$ROOTFS" "$@"
