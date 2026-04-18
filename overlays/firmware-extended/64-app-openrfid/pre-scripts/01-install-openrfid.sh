7ecbd6bfad3e598a42e13237dc722b1ad83c95ef
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2026-04-18 22:11:29 +0200

    Bump OpenRFID version

--- overlays/firmware-extended/64-app-openrfid/pre-scripts/01-install-openrfid.sh
+++ overlays/firmware-extended/64-app-openrfid/pre-scripts/01-install-openrfid.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 GIT_URL=https://github.com/suchmememanyskill/OpenRFID.git
-GIT_SHA=a45aacd7283be88126f0551052fd3baede9da057
+GIT_SHA=f209311bb5cb75281d7923e5401ada68ab62ffc3
 
 if [[ -z "$CREATE_FIRMWARE" ]]; then
   echo "Error: This script should be run within the create_firmware.sh environment."
